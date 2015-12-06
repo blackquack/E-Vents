e85c9fabab637dc9f9f4c27ab2425696bd94632e
mrweili <mrwei.li@mail.utoronto.ca>
2015-12-06 17:58:54 -0500

    fix profile msging & redirect if no exist

--- E-vents/client/ctrl_profile.js
+++ E-vents/client/ctrl_profile.js
@@ -1,13 +1,47 @@
 angular.module('app').controller('profileController',
-['$scope', '$location', 'AuthService', '$routeParams', 'UserService',
-function ($scope, $location, AuthService, $routeParams, UserService) {
+['$scope', '$location', 'AuthService', '$routeParams', 'UserService', '$mdDialog', '$mdMedia',
+function ($scope, $location, AuthService, $routeParams, UserService, $mdDialog, $mdMedia) {
     $scope.user = AuthService.getUserInfo();
     $scope.greet = "Profile Page";
 
+    /* GOTO HOME IF NO USER EXIST, ALSO SET EVENTS */
     UserService.getEvents.get({user: $routeParams.username}, 
     function(result){
+        if (result._id == null) $location.path('/')
         $scope.events = result.attendance;
     });
+
+
+    /* SET THE MESSAGE BUTTON */
+    $scope.showDialog = showDialog;
+
+    /* MESSAGE SOMEONE BUTTON DIALOG FUNCTIONALIY*/
+    function showDialog($event) {
+        var parentEl = angular.element(document.body);
+        $mdDialog.show({
+            parent: parentEl,
+            targetEvent: $event,
+            templateUrl: "views/dialog.html",
+            controller: DialogController
+        });
+
+        function DialogController($scope, $mdDialog) {
+            $scope.receiver = $routeParams.username
+
+            $scope.cancel = function() {
+                $mdDialog.hide();
+            }
+
+            $scope.send = function() {
+                if ($scope.receiver == null || $scope.receiver == '') return
+                if ($scope.content == null || $scope.content == '') return
+
+                sendMessage($scope.receiver, $scope.content)
+                $mdDialog.hide();
+            }
+
+        }
+    }
 
 }]);
 
